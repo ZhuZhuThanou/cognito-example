8f3359efde68ce62cc05a628f4c2b6bd7214452b
Thanou <blogicalbmath@gmail.com>
2020-10-12 20:26:02 -0400

    add deploy util

---
+++ deploy.js
@@ -0,0 +1,42 @@
+const fileSystem = require('fs');
+const archiver = require('archiver');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+  accessKeyId: 'xx',
+  secretAccessKey: 'xx'
+});
+
+const fileContent = fileSystem.readFileSync('angular-target.zip');
+const params = {
+  Bucket: 'sample-build-coco',
+  Key: 'angular-target.zip', // File name you want to save as in S3
+  Body: fileContent
+};
+
+
+const output = fileSystem.createWriteStream('angular-target.zip');
+const archive = archiver('zip');
+
+
+output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+    s3.upload(params, function(err, data) {
+      if (err) {
+          throw err;
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+  });
+});
+
+archive.on('error', function(err){
+    throw err;
+});
+
+archive.pipe(output);
+
+// append files from a sub-directory, putting its contents at the root of archive
+archive.directory('./dist', false);
+
+archive.finalize();
